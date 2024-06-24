99904791654156df6404d33272dd3e0e6acac9cd
Andresit0 <40771091+Andresit0@users.noreply.github.com>
2024-06-24 09:37:58 -0500

    Update latex.dart to Flutter 3.22.2

--- latex/lib/latex.dart
+++ latex/lib/latex.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_tex/flutter_tex.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 TeXViewWidget introduction =
-    _teXViewWidget(r"""<h4>Flutter \( \rm\\TeX \)</h4>""", r""" 
+    _teXViewWidget(r"""<h4>Flutter \( \rm\\TeX \)</h4>""", r"""
        
       <p>Flutter \( \rm\\TeX \) is a Flutter Package to render so many types of equations based on \( \rm\\LaTeX \), It also includes full HTML with JavaScript
       support.</p>
@@ -31,7 +30,7 @@ TeXViewWidget align = _teXViewWidget("Align", r"""
     $$
     </p>""");
 
-TeXViewWidget alignedTag = _teXViewWidget(r"<h4>Table</h4>", r"""    
+TeXViewWidget alignedTag = _teXViewWidget(r"<h4>Table</h4>", r"""
            $$
            \begin{array}{|c|c|}
               \hline
@@ -45,11 +44,11 @@ TeXViewWidget alignedTag = _teXViewWidget(r"<h4>Table</h4>", r"""
            $$""");
 
 TeXViewWidget image() {
-  return TeXViewContainer(
+  return const TeXViewContainer(
     child: TeXViewImage.network(
         'https://raw.githubusercontent.com/shah-xad/flutter_tex/master/example/assets/flutter_tex_banner.png'),
     style: TeXViewStyle(
-      textAlign: TeXViewTextAlign.Center,
+      textAlign: TeXViewTextAlign.center,
       height: 300,
       margin: TeXViewMargin.all(10),
       borderRadius: TeXViewBorderRadius.all(20),
@@ -58,44 +57,44 @@ TeXViewWidget image() {
 }
 
 TeXViewWidget video() {
-  return TeXViewContainer(
+  return const TeXViewContainer(
     child: TeXViewVideo.youtube(
         "https://www.youtube.com/watch?v=YiNbVEXV_NM&lc=Ugyg4ljzrK0D6YfrO854AaABAg"),
     style: TeXViewStyle(
-      textAlign: TeXViewTextAlign.Center,
+      textAlign: TeXViewTextAlign.center,
       width: 300,
     ),
   );
 }
 
-TeXViewStyle _teXViewStyle = TeXViewStyle(
-  textAlign: TeXViewTextAlign.Center,
+TeXViewStyle _teXViewStyle = const TeXViewStyle(
+  textAlign: TeXViewTextAlign.center,
   margin: TeXViewMargin.all(10),
   padding: TeXViewPadding.all(10),
   borderRadius: TeXViewBorderRadius.all(10),
   border: TeXViewBorder.all(
     TeXViewBorderDecoration(
         borderColor: Colors.blue,
-        borderStyle: TeXViewBorderStyle.Solid,
+        borderStyle: TeXViewBorderStyle.solid,
         borderWidth: 2),
   ),
 );
 
 TeXViewWidget onTap() {
   return TeXViewInkWell(
-      child: TeXViewDocument(r""" Button (More Information) """),
+      child: const TeXViewDocument(r""" Button (More Information) """),
       style: _teXViewStyle,
       id: "inkwell_1",
       rippleEffect: true,
       onTap: (id) async {
-        print(id);
-
         //Using url_launcher: ^6.0.8 (Flutter Package)
         const url = "https://katex.org/docs/support_table.html#i";
-        if (await canLaunch(url))
-          await launch(url);
-        else
+        Uri uri = Uri.parse(url);
+        if (await canLaunchUrl(uri)) {
+          await launchUrl(uri);
+        } else {
           throw "Could not launch $url";
+        }
       });
 }
 
@@ -104,18 +103,18 @@ TeXViewWidget _teXViewWidget(String title, String body) {
     TeXViewDocument(title,
         style: TeXViewStyle(
             contentColor: Colors.blue,
-            textAlign: TeXViewTextAlign.Center,
+            textAlign: TeXViewTextAlign.center,
             fontStyle: TeXViewFontStyle(fontSize: 20))),
     TeXViewDocument(body,
-        style: TeXViewStyle(margin: TeXViewMargin.only(top: 10)))
+        style: const TeXViewStyle(margin: TeXViewMargin.only(top: 10)))
   ]);
 }
 
 class TeXViewDocumentExamples extends StatelessWidget {
   final TeXViewRenderingEngine renderingEngine;
 
-  TeXViewDocumentExamples(
-      {this.renderingEngine = const TeXViewRenderingEngine.katex()});
+  const TeXViewDocumentExamples(
+      {super.key, this.renderingEngine = const TeXViewRenderingEngine.katex()});
 
   @override
   Widget build(BuildContext context) {
